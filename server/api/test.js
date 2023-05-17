c845608b54d61f38b1d6ea2bbdcfb3d781017894
Muhammad Nadeem Abbasi <58105289+nadeemciit102@users.noreply.github.com>
2023-05-18 03:13:29 +0400

    Remove the API key from test.js

--- server/api/test.js
+++ server/api/test.js
@@ -9,7 +9,7 @@
 // });
 export default defineEventHandler(async () => {
   //const apiKey = "76281adefd1be8ab371c5a756dc5910d";
-  const { data } = await $fetch("https://jsonplaceholder.typicode.com/todos/1"); //https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=76281adefd1be8ab371c5a756dc5910d
+  const { data } = await $fetch("https://jsonplaceholder.typicode.com/todos/1"); //https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=
 
   return data || "Your API is Unable to Fetch data. Please check later...";
 });
